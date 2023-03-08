e752b8ec40c57295ddc5c14db82d86564a4b659d
Fox2low <77130209+Fox2low@users.noreply.github.com>
2023-03-08 18:06:42 +1100

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -5,30 +5,24 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 import DB
 
-# connect to database and load historical data
 data = DB.get_plant_data()
 
-# preprocess data
 data = data.dropna()
 data = data.drop_duplicates()
 data = data.reset_index(drop=True)
 
-# extract features and labels
 X = data[['temp_air', 'temp_soil', 'moisture_air', 'moisture_soil', 'light_level']]
 y = data['needs_water']
-# split data into training and testing sets
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# train decision tree classifier
 clf = DecisionTreeClassifier(random_state=42)
 clf.fit(X_train, y_train)
 
-# evaluate accuracy of classifier
 y_pred = clf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
-# predict if plant needs watering in next hour
 temp_air = db_conn.get_latest_value('temp_air')
 temp_soil = db_conn.get_latest_value('temp_soil')
 moisture_air = db_conn.get_latest_value('moisture_air')
